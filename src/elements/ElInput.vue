a4564abbc474894122fcb7139d3b58d81c5f3855
Vigriyanov Alexandr <lexnnov2009@gmail.com>
2021-12-02 00:17:58 +0300

    брал возможность вводить точку

--- src/elements/ElInput.vue
+++ src/elements/ElInput.vue
@@ -36,7 +36,7 @@
       },
       onlyNumber($event) {
         let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
-        if ((keyCode < 48 || keyCode > 57) && keyCode !== 46) { // 46 is dot
+        if (keyCode < 48 || keyCode > 57) {
           $event.preventDefault();
         }
       }
